82d033357400a2fa057c69804b7da496d5804b5b
Youssef-Taqi-Eldeen <taqieldeenyoussef@gmail.com>
2024-10-13 21:49:38 +0300

    create custom note item

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 12),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 32,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 30),
+            child: Text(
+              'may21 , 2022',
+              style: TextStyle(color: Colors.black.withOpacity(0.5)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
